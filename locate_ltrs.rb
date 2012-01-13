767d49d31b3bab330ba4297f298ce29ca3036c9f
Evan Senter <evansenter@gmail.com>
2012-01-13 14:11:20 -0500

    Properly complement putative ERVs from the minus strand.

--- locate_ltrs.rb
+++ locate_ltrs.rb
@@ -1,37 +1,41 @@
 require "bio"
 require "entrez"
-require "nokogiri"
 
 class BlastParser
   FLANKING_DISTANCE = 5_000
   ERV_DISTANCE      = 4_000..10_000
   
-  attr_reader :xml, :query_length
+  attr_reader :report
 
   def self.bootstrap(path)
-    new(Nokogiri::XML(File.open(path)))
+    new(Bio::Blast::Report.new(File.read(path)))
   end
   
-  def initialize(xml)
-    @xml          = xml
-    @query_length = xml.xpath("BlastOutput/BlastOutput_query-len").inner_text.to_i
+  def initialize(report)
+    @report = report
   end
   
   def parse_xml
-    xml.xpath("//BlastOutput_iterations//Iteration//Iteration_hits//Hit").map do |hit_group|
+    report.hits.map do |hit|
       {
-        id:        hit_group.xpath("Hit_id").inner_text.split("|")[1],
-        accession: hit_group.xpath("Hit_accession").inner_text,
-        locations: parse_coordinates(hit_group)
+        definition: hit.definition,
+        id:         hit.hit_id.split("|")[1],
+        accession:  hit.accession,
+        locations:  parse_coordinates(hit.hsps.select { |hsp| hsp.align_len > report.query_len * 0.9 })
       }
     end
   end
   
   def filter
     parse_xml.map do |hash|
-      hash.merge(ltr_pairs: filter_array(hash[:locations][:plus]) + filter_array(hash[:locations][:minus]))
+      hash.merge({
+        ltr_pairs: {
+          plus:  filter_array(hash[:locations][:plus]),
+          minus: filter_array(hash[:locations][:minus])
+        }
+      })
     end.reject do |hash|
-      hash[:ltr_pairs].empty?
+      hash[:ltr_pairs][:plus].empty? && hash[:ltr_pairs][:minus].empty?
     end
   end
   
@@ -43,21 +47,30 @@ class BlastParser
   
   private
   
-  def flanking_pairs(id, pairs_array)
-    pairs_array.map do |putative_erv|
-      {
-        coordinates: putative_erv,
-        flanking_5:  sequence_from_entrez(id, putative_erv.first.begin - FLANKING_DISTANCE, putative_erv.first.begin - 1),
-        flanking_3:  sequence_from_entrez(id, putative_erv.last.end + 1, putative_erv.last.end + FLANKING_DISTANCE)
-      }
-    end
+  def flanking_pairs(id, pairs_hash)
+    {
+      plus: pairs_hash[:plus].map do |putative_erv|
+        {
+          putative_erv: putative_erv,
+          flanking_5:  sequence_from_entrez(id, putative_erv.first.hit_from, -FLANKING_DISTANCE..-1),
+          flanking_3:  sequence_from_entrez(id, putative_erv.last.hit_to, 1..FLANKING_DISTANCE)
+        }
+      end,
+      minus: pairs_hash[:minus].map do |putative_erv|
+        {
+          putative_erv: putative_erv,
+          flanking_5:  Bio::Sequence::NA.new(sequence_from_entrez(id, putative_erv.last.hit_to, 1..FLANKING_DISTANCE)).complement,
+          flanking_3:  Bio::Sequence::NA.new(sequence_from_entrez(id, putative_erv.first.hit_from, -FLANKING_DISTANCE..-1)).complement
+        }
+      end
+    }
   end
   
-  def sequence_from_entrez(id, start, stop)
+  def sequence_from_entrez(id, position, window)
     fasta = Entrez.EFetch("nuccore", {
       id:        id, 
-      seq_start: start, 
-      seq_stop:  stop, 
+      seq_start: position + window.begin, 
+      seq_stop:  position + window.end, 
       retmode:   :fasta, 
       rettype:   :text
     }).response.body
@@ -65,31 +78,29 @@ class BlastParser
     Bio::FastaFormat.new(fasta).seq
   end
   
-  def filter_array(coordinate_array)
-    coordinate_array.combination(2).select do |lower, upper|
-      ERV_DISTANCE.include?(upper.begin - lower.end)
+  def filter_array(hsps)
+    hsps.combination(2).select do |match_5, match_3|
+      ERV_DISTANCE.include?(match_3.hit_from - match_5.hit_to)
     end
   end
   
-  def parse_coordinates(hit_group)
-    partitioned_coordinates = hit_group.xpath("Hit_hsps/Hsp").map do |hit|
-      [hit.xpath("Hsp_hit-from").inner_text.to_i, hit.xpath("Hsp_hit-to").inner_text.to_i]
-    end.select do |coordinates|
-      (coordinates.last - coordinates.first).abs > query_length * 0.9
-    end.partition do |coordinates|
-      coordinates.last > coordinates.first
-    end.map do |array|
-      array.map { |coordinates| Range.new(*coordinates.sort) }.sort_by(&method(:midpoint))
+  def parse_coordinates(hsps)
+    partitioned_hsps = hsps.partition(&method(:same_strand?)).map do |hsps|
+      hsps.sort_by(&method(:midpoint)) #{ |a, b| midpoint(a) <=> midpoint(b) * (same_strand?(a) ? 1 : -1) }
     end
     
-    Hash[[:plus, :minus].zip(partitioned_coordinates)]
+    Hash[[:plus, :minus].zip(partitioned_hsps)]
   end
   
-  def midpoint(range)
-    (range.begin + range.end) / 2.0
+  def midpoint(hsp)
+    (hsp.hit_from + hsp.hit_to) / 2.0
+  end
+  
+  def same_strand?(hsp)
+    hsp.query_frame == hsp.hit_frame
   end
 end
 
 parser             = BlastParser.bootstrap("/Users/evansenter/Documents/School/BC/Rotation 3 - Johnson/Canis Lupus Familiaris/GY1D8VT9016-Alignment.xml")
 ltr_pairs          = parser.filter
-flanking_sequences = parser.flanking_regions!
+flanking_sequences = parser.flanking_regions!.map { |hash| hash[:flanking_pairs] }
