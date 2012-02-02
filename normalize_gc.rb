922ef1c2d1a89c1584d08c0bf5781f3d50ef38d3
Evan Senter <evansenter@gmail.com>
2012-02-02 15:51:08 -0500

    Normalizer works.

--- normalize_gc.rb
+++ normalize_gc.rb
@@ -1,74 +1,93 @@
-raise "I DON'T THINK THIS WORKS"
-
 require "bio"
 require "./fasta_printer.rb"
 
-class NormalizeGc
-  attr_reader :folders, :orf_suffix, :window_size, :flanking_length
+class GcRunner
+  WINDOW_DOMAIN = -5000..-1
   
-  def initialize(directory, orf_suffix = "env", window_size = 1_500, flanking_length = 5_000)
-    @folders         = Dir[File.join(directory, "*")]
-    @orf_suffix      = orf_suffix
-    @window_size     = window_size
-    @flanking_length = flanking_length
-  end
+  attr_reader :folders
   
-  def write_best_windows!
-    folders.map(&method(:write_best_window_by_gc_content))
-  end
-  
-  def write_best_window_by_gc_content(folder)
-    target_gc_content  = load_from_folder(folder, orf_suffix).gc_content.to_f
-    erv_sequence       = load_from_folder(folder, "erv")[-5000..-1]
-    best_sequence_hash = find_best_sequence(erv_sequence, target_gc_content)
-    
-    GcResult.new(target_gc_content, best_sequence_hash).write_fasta!(folder)
-  end
-  
-  def find_best_sequence(flanking_region, target_gc_content)
-    matches = (0..(flanking_length - window_size)).map do |i|
-      sequence = flanking_region[i, window_size]
-      
-      {
-        index:                 i,
-        sequence:              sequence,
-        gc_content_difference: (target_gc_content - sequence.gc_content.to_f).abs
-      }
-    end
-      
-    matches.min { |a, b| a[:gc_content_difference] <=> b[:gc_content_difference] }
+  def initialize(directory)
+    @folders = Dir[File.join(directory, "*")]
   end
   
   def load_from_folder(folder, suffix)
     file_path = Dir[File.join(folder, "*#{suffix}.txt")].first
+    sequence  = Bio::Sequence::NA.new(Bio::FastaFormat.new(File.read(file_path).gsub(/\r/, "\n")).seq)
     
-    Bio::Sequence::NA.new(Bio::FastaFormat.new(File.read(file_path)).seq)
+    sequence.empty? ? (raise "Empty sequence in folder #{folder} with suffix #{suffix}") : sequence
   end
   
-  class GcResult
-    include FastaPrinter
+  def window(folder)
+    gc_sequence   = load_from_folder(folder, "env")
+    window_domain = load_from_folder(folder, "erv")
     
-    attr_reader :target_gc_content, :best_sequence_hash
-    
-    def initialize(target_gc_content, best_sequence_hash)
-      @target_gc_content  = target_gc_content
-      @best_sequence_hash = best_sequence_hash
+    GcWindow.new(gc_sequence, window_domain[WINDOW_DOMAIN])
+  end
+  
+  def write_best_windows!
+    folders.map do |folder|
+      window(folder).write_best_window!(folder)
     end
-    
-    def fasta_hash
+  end
+end
+
+class GcWindow
+  WINDOW_SIZE = 1500
+  
+  attr_reader :gc_sequence, :window_domain
+  
+  def initialize(gc_sequence, window_domain)
+    @gc_sequence, @window_domain = gc_sequence, window_domain
+  end
+  
+  def target_gc_content
+    @sequence_content ||= gc_sequence.gc_content.to_f
+  end
+  
+  def best_window
+    window_hashes.min { |a, b| a[:gc_difference] <=> b[:gc_difference] }
+  end
+  
+  def window_hashes
+    (0..(window_domain.length - WINDOW_SIZE)).map do |i|
+      sequence = window_domain[i, WINDOW_SIZE]
+      
       {
-        comment: "> Deviated from GC content of %.2f by %.2f percent, starts in 3' genomic region at position %d (0-indexed)" % [
-          target_gc_content * 100,
-          best_sequence_hash[:gc_content_difference] * 100,
-          best_sequence_hash[:index]
-        ],
-        sequence: best_sequence_hash[:sequence]
+        index:         i + 1,
+        sequence:      sequence,
+        gc_difference: (target_gc_content - sequence.gc_content).abs
       }
     end
-    
-    def fasta_filename
-      "Highest matching GC region.fasta"
-    end
+  end
+  
+  def write_best_window!(directory)
+    GcPrinter.new(target_gc_content, best_window).write_fasta!(directory)
+  end
+end
+
+class GcPrinter
+  include FastaPrinter
+  
+  attr_reader :target_gc_content, :best_sequence_hash
+  
+  def initialize(target_gc_content, best_sequence_hash)
+    @target_gc_content  = target_gc_content
+    @best_sequence_hash = best_sequence_hash
+  end
+  
+  def fasta_hash
+    {
+      comment: "> Deviated from GC content of %.2f by %.2f percent, starts in 3' genomic region at position %d (1-indexed)" % [
+        target_gc_content * 100,
+        best_sequence_hash[:gc_difference] * 100,
+        best_sequence_hash[:index]
+      ],
+      sequence: best_sequence_hash[:sequence]
+    }
+  end
+  
+  def fasta_filename
+    "Highest matching GC region.fasta"
   end
 end
 
