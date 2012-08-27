7a8361143fd2db461d65b63e63d5af7ec45ed9e4
Evan Senter <evansenter@gmail.com>
2012-08-27 14:35:57 -0400

    Script takes erv or ltr at the command line.

--- find_erv.rb
+++ find_erv.rb
@@ -1,10 +1,14 @@
 require "./finderv.rb"
 
-if ARGV.length == 1
-  puts "Running the following files:"
-  Dir[File.join(ARGV[0], "*.xml")].each { |filepath| puts("\t%s" % filepath) }
+if ARGV.length == 2 && ARGV[0] =~ /^(erv|ltr)$/i
+  puts "Scanning the following files for #{ARGV[0].downcase}:"
+  (files = Dir[File.join(ARGV[1], "*.xml")]).each { |filepath| puts("\t%s" % filepath) }
   
-  Finderv.write_putative_erv_batch!(Finderv.putative_ervs_from_batch(Dir[File.join(ARGV[0], "*.xml")]), ARGV[0])
+  if ARGV[0] =~ /erv/i
+    Finderv.write_putative_erv_batch!(Finderv.putative_ervs_from_batch(files), ARGV[1])
+  elsif ARGV[0] =~ /ltr/i
+    Finderv.write_solo_ltr_batch!(Finderv.solo_ltrs_from_batch(files), ARGV[1])
+  end
 else
-  puts "ruby find_erv.rb DIRECTORY_WITH_XML_FILES"
+  puts "ruby find_erv.rb (erv OR ltr) DIRECTORY_WITH_XML_FILES"
 end
